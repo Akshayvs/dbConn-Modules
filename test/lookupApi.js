18344e31f58868324498357ad5f9200ea04c6bc0
Akshay Sonawane <akshay.vs9543@gmail.com>
2016-06-30 19:55:10 -0400

    Changed Code Format. Changed structure of modules, everything is working.
    Successful Transformer Test

--- test/lookupApi.js
+++ test/lookupApi.js
@@ -8,10 +8,8 @@ var assert = require('chai').assert;
 describe('LookupApi', function () {
 
     var lookupApi;
-
     var serverStub;
     var getStub;
-
     var getDoccMock;
 
     before(function () {
@@ -19,8 +17,10 @@ describe('LookupApi', function () {
         mockery.registerAllowable('../lib/metrics-collection.js');
 
         serverStub ={
-            get: getStub =sinon.stub().returns({ })
+            get: getStub =sinon.stub()
         }
+
+        getDoccMock = sinon.stub().returns()
 
     });
 
@@ -30,27 +30,46 @@ describe('LookupApi', function () {
     });
 
     beforeEach(function () {
-        
         mockery.registerMock('./getDoc', getDoccMock);
         lookupApi=require('../lib/lookupApi.js');
-
-        getDoccMock = sinon.stub().returns({})
-
 
     });
 
 
     afterEach(function () {
         mockery.resetCache();
-        mockery.deregisterMock('nimbus-framework');
+        mockery.deregisterMock('./getDoc');
     });
 
-    it('callback should send an 404 response when err is passed', function() {
+
+    it('checks if get function is called', function() {
 
         lookupApi.init(serverStub);
-
         assert.equal(getStub.callCount,1);
     })
 
+    it('checks if a valid 1st parameter is passed to \'get\'', function() {
+        lookupApi.init(serverStub);
+        assert(getStub.calledWith('/getDoc/:key'));
+
+    })
+
+    it('checks if a request is received', function() {
+        var searchKey = 'some random key';
+        var fieldsRequested='1stkey, 2ndKey, 3rdKey';
+        var key=sinon.stub();
+
+        var request= {
+            params: {
+                fieldsRequested: fieldsRequested,
+                key: searchKey
+            }
+        }
+
+        getStub.callsArgWith(1,request);
+        lookupApi.init(serverStub);
+        //assert.equal(getDoccMock.calledWith(searchKey, fieldsRequested.split(',')), true);
+        expect(getDoccMock.calledWith(searchKey, fieldsRequested.split(','))).to.be.true;
+    })
 })
 
